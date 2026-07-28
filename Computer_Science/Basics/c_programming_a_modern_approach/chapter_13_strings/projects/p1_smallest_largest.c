cfd5a8fcf5e7ce65e1edb95334f0c7093493cdc4
Filip Matura <matura.filip96@gmail.com>
2026-07-28 16:33:12 +0200

    project 1 chapter 13 added

--- Computer_Science/Basics/c_programming_a_modern_approach/chapter_13_strings/projects/p1_smallest_largest.c
+++ Computer_Science/Basics/c_programming_a_modern_approach/chapter_13_strings/projects/p1_smallest_largest.c
@@ -11,27 +11,51 @@
 #include <stdio.h>
 #include <string.h>
 
-#define WORDS 3
-#define WORD_LEN 10
+
+#define WORD_LEN 20
+#define EXIT_WORD_LEN 4
 
 
 int read_line(char str[], int n);
 
 int main(void) {
-    char *animals[WORDS];
-    
-    // create smallest and largest strings
 
-    // ask repeatedly user for word
-        // check if the word is len 4 => break
-        // compare strcmp(input, smallest/largest)
+    // load smallest and largest
+    char smallest[WORD_LEN + 1];
+    char largest[WORD_LEN + 1];
+    for (int i = 0; i < WORD_LEN; i++) {
+        smallest[i] = 'z';
+        largest[i] = 'a';
+    }
+    smallest[WORD_LEN] = 0;
+    largest[WORD_LEN] = 0;
 
-    // print smallest largest
+    // reading user words
+    char user_word[WORD_LEN + 1];
+    while (1) {
+        printf("Enter word: ");
+        read_line(user_word, WORD_LEN + 1);
 
+        // check "largest" and "smallest"
+        if (strcmp(user_word, smallest) < 0) {
+            strcpy(smallest, user_word);
+        }
+
+        if (strcmp(user_word, largest) > 0) {
+            strcpy(largest, user_word);
+        }
+
+        // exit the program and show the "largest" and "smallest"
+        if (strlen(user_word) == EXIT_WORD_LEN) {
+            break;
+        }
+    }
+
+    printf("Smallest word: %s\n", smallest);
+    printf("Largest word: %s\n", largest);
 
     return 0;
 }
-
 
 int read_line(char str[], int n) {
     int ch, i = 0;
