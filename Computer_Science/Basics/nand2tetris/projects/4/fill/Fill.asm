2183daa4a7b5d8ebb90745d585df56ffe2d3bf04
Filip Matura <matura.filip96@gmail.com>
2026-08-08 09:40:12 +0200

    fill screen project 4 added

--- Computer_Science/Basics/nand2tetris/projects/4/fill/Fill.asm
+++ Computer_Science/Basics/nand2tetris/projects/4/fill/Fill.asm
@@ -7,4 +7,60 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+@SCREEN
+D=A
+@addr
+M=D     // addr = 16_384 screen's base address
+
+
+(WHITE_LOOP)
+    @addr
+    A=M
+    M=0    // RAM[addr] = 0
+
+    @1
+    D=A
+    @addr
+    M=D+M   // addr = addr + 1
+    @KBD
+    D=M
+    @RESET_BLACK
+    D;JNE
+    @WHITE_LOOP
+    0;JMP
+
+
+(BLACK_LOOP)
+    @addr
+    A=M
+    M=-1    // RAM[addr] = -1 aka 1111111111111111 (BLACK)
+
+    @1
+    D=A
+    @addr
+    M=D+M   // addr = addr + 1
+    
+    @KBD
+    D=M
+    @BLACK_LOOP
+    D;JNE
+    @RESET_WHITE
+    0;JMP
+
+(RESET_BLACK)
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    @BLACK_LOOP
+    0;JMP
+
+(RESET_WHITE)
+    @SCREEN
+    D=A
+    @addr
+    M=D 
+    @WHITE_LOOP
+    0;JMP
+
