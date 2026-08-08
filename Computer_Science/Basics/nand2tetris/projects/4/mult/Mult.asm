fd64c72ad5ec251057c30a895de77510667bf554
Filip Matura <matura.filip96@gmail.com>
2026-08-08 08:41:12 +0200

    mult.asm for project 4 added

--- Computer_Science/Basics/nand2tetris/projects/4/mult/Mult.asm
+++ Computer_Science/Basics/nand2tetris/projects/4/mult/Mult.asm
@@ -6,4 +6,69 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// RAM[0] = some number, RAM[1] = some number
+// RAM[2] = RAM[0] * RAM[1]
+// assumptions: RAM[0] and RAM[0] > 0 and < 32_768
+
+/*
+int main()
+{
+   int ram_1 = 2;
+   int ram_2 = 3;
+   int n = ram_1;
+   int i = 0;
+   int sum = 0;
+   for (; i < n; i++) {
+       sum += ram_2;
+   }
+   printf("%d\n", sum);
+
+    return 0;
+}
+*/
+
+// initialize variables
+@R0
+D=M
+@n
+M=D // n = R0
+
+@sum
+M=0
+
+@i
+M=0
+
+(LOOP)
+    // for (i = 0; i < n; i++
+    @i
+    D=M
+    @n
+    D=D-M
+    @STOP
+    D;JEQ
+
+    @R1
+    D=M
+    @sum
+    D=D+M
+    M=D
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+
+(STOP)
+    @sum
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
+
+
+
